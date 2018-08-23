02dac7fe2c56ebcc7e12b706c6ffb1aace63aeb8
“JaeCoding” <641403035@qq.com>
2018-08-23 23:31:04 +0800

    updated in 2018-8-23 23:30:59

--- src/main/java/com/imooc/miaosha/config/WebConfig.java
+++ src/main/java/com/imooc/miaosha/config/WebConfig.java
@@ -9,6 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import java.util.List;
 
+
+/**
+ * mvc 的相关配置
+ */
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter{
 
@@ -20,7 +24,7 @@ public class WebConfig extends WebMvcConfigurerAdapter{
     AccessInterceptor accessInterceptor;
 
     /**
-     * 这个方法，实现了controller中参数可直接添加诸如model，request等。同样的，可以用他来实现添加 user
+     * 这个方法，实现了controller中参数可直接添加 诸如model，request等。同样的，可以用他来实现添加 user
      * @param argumentResolvers 是个List<HandlerMethodArgumentResolver>，所以我们把需要实现的UserArgumentResolvers加进去
      */
     @Override
@@ -30,6 +34,7 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 
     /**
      * 拦截器的注册  WebMvcConfigurerAdapter中的addInterceptors方法
+     * 注册上我们 自定义的拦截器
      * @param registry
      */
     @Override
